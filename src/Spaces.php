2b61603d329ad4a2a3be13175b93587b7423e470
Fredric Lövefors <fredric@panang.se>
2020-11-09 17:43:20 +0100

    Add static make method

--- src/Spaces.php
+++ src/Spaces.php
@@ -18,6 +18,27 @@ class Spaces
         $this->bucket = $bucket;
     }
 
+    public static function make(array $config) : Spaces
+    {
+        foreach (['key', 'secret', 'region', 'bucket', 'endpoint'] as $key) {
+            if (! isset($config[$key])) {
+                throw new \InvalidArgumentException("The config is missing key {$key}");
+            }
+        }
+
+        return new static(
+            new S3Client([
+                'version' => 'latest',
+                'region'  => $config['region'],
+                'endpoint' => $config['endpoint'],
+                'credentials' => [
+                    'key'    => $config['key'],
+                    'secret' => $config['secret']
+                ]
+            ], $config['bucket'])
+        );
+    }
+
     public function sign(?string $contentType = null)
     {
         $signedRequest = $this->signRequest(
